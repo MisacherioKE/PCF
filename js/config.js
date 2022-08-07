d0b2130e22d1ee4de1b37655d47cb19ff5c4c036
Carlos Misachi <calphonce31@gmail.com>
2022-08-07 23:15:55 +0300

    updates

---
+++ js/config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDjaS1LFpuvtsRUQh59RYrNtJkmBbL435o",
+    authDomain: "pcfweb-46709.firebaseapp.com",
+    projectId: "pcfweb-46709",
+    storageBucket: "pcfweb-46709.appspot.com",
+    messagingSenderId: "768204127195",
+    appId: "1:768204127195:web:8ff4e58fa63569bbcf05fe",
+    measurementId: "G-M2T6G3TVGS"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);

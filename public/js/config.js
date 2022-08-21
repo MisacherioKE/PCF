9fbdf8670c9ef9a37f1b9c6ea374a4ba5d54c8eb
Carlos Misachi <calphonce31@gmail.com>
2022-08-21 22:25:07 +0300

    updates

--- public/js/config.js
+++ public/js/config.js
@@ -1,13 +1,13 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDjaS1LFpuvtsRUQh59RYrNtJkmBbL435o",
-    authDomain: "pcfweb-46709.firebaseapp.com",
-    projectId: "pcfweb-46709",
-    storageBucket: "pcfweb-46709.appspot.com",
-    messagingSenderId: "768204127195",
-    appId: "1:768204127195:web:8ff4e58fa63569bbcf05fe",
-    measurementId: "G-M2T6G3TVGS"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+  apiKey: "AIzaSyDi8Rnn1mScQcAkOJq6mAYH8xo8b2SOKSQ",
+  authDomain: "pcfwebsite-487cd.firebaseapp.com",
+  projectId: "pcfwebsite-487cd",
+  storageBucket: "pcfwebsite-487cd.appspot.com",
+  messagingSenderId: "979073085844",
+  appId: "1:979073085844:web:ec2297839f785a0bdc9f81",
+  measurementId: "G-S8L450DBVC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);

f5ddb9e2c4b071adad1d44bbb0dac1d9c63bca5e
Carlos Misachi <calphonce31@gmail.com>
2022-08-24 19:52:30 +0300

    updates

--- public/js/config.js
+++ public/js/config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBPvFWKgyPVwM3LXUWjECoPnk-qaL35Dgw",
-  authDomain: "pcare-322da.firebaseapp.com",
-  projectId: "pcare-322da",
-  storageBucket: "pcare-322da.appspot.com",
-  messagingSenderId: "440536641372",
-  appId: "1:440536641372:web:e5ebebe28b2f21a4e7db99",
-  measurementId: "G-SZB0WKCGCD"
+  apiKey: "AIzaSyA6HI_qBQpi9v1bO-AEB18jYmdE9_p55dY",
+  authDomain: "pillarscare-8b95d.firebaseapp.com",
+  projectId: "pillarscare-8b95d",
+  storageBucket: "pillarscare-8b95d.appspot.com",
+  messagingSenderId: "970590895633",
+  appId: "1:970590895633:web:c88c200bced7c945d37bb0",
+  measurementId: "G-CR4TJP5CBY"
 };
 
 // Initialize App

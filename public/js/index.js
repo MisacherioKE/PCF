a849703356d70b549f58d4e725ca030950dca865
Carlos Misachi <calphonce31@gmail.com>
2022-09-03 10:43:07 +0300

    updates

--- public/js/index.js
+++ public/js/index.js
@@ -18,3 +18,27 @@ document.getElementById("menuItem").onmouseover =()=>{
 document.getElementById("btnInvolve").onclick = ()=>{
     window.location.href = "/public/html/getInvolved.html";
 }
+
+
+
+// Gallery slide
+var slideShow = "orange";
+function change(){
+    var image =document.getElementById('slide');
+    if(slideShow == 'orange'){
+        image.src='/public/images/event3.jpeg';
+        slideShow ='blue';
+    }
+    else{
+        image.src ="/public/images/background.jpg";
+        slideShow='orange';
+    }
+    
+}
+var timer = setInterval (()=>{
+    change();
+},2000);
+
+function stop (){
+    clearInterval(timer);
+}

b8a41e01f3da5e21be773423568a08d377360ed9
Carlos Misachi <calphonce31@gmail.com>
2022-09-03 10:59:50 +0300

    updates

--- public/js/index.js
+++ public/js/index.js
@@ -22,23 +22,44 @@ document.getElementById("btnInvolve").onclick = ()=>{
 
 
 // Gallery slide
-var slideShow = "orange";
-function change(){
-    var image =document.getElementById('slide');
-    if(slideShow == 'orange'){
-        image.src='/public/images/event3.jpeg';
-        slideShow ='blue';
-    }
-    else{
-        image.src ="/public/images/background.jpg";
-        slideShow='orange';
-    }
+// var slideShow = "orange";
+// function change(){
+//     var image =document.getElementById('slide');
+//     if(slideShow == 'orange'){
+//         image.src='/public/images/event3.jpeg';
+//         slideShow ='blue';
+//     }
+//     else{
+//         image.src ="/public/images/background.jpg";
+//         slideShow='orange';
+//     }
     
-}
-var timer = setInterval (()=>{
-    change();
-},2000);
+// }
+// var timer = setInterval (()=>{
+//     change();
+// },2000);
 
-function stop (){
-    clearInterval(timer);
+// function stop (){
+//     clearInterval(timer);
+// }
+var i =0;
+var images =[];
+var time = 3000;
+
+images[0]='/public/images/event3.jpeg';
+images[1]='/public/images/youthRep.jpeg';
+images[2]='/public/images/offtake.jpeg';
+images[3]='/public/images/kids.jpeg';
+
+function changeImg (){
+    // document.getElementById('slide').src = images[i];
+    document.slide.src = images[i];
+    if( i < images.length - 1){
+        i++;
+    }else{
+        i=0;
+    }
+
+    setTimeout('changeImg()', time);
 }
+window.onload = changeImg;
